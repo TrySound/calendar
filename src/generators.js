31a31224f7e06ab0b3bd5099593e8b7cfd46a948
Bogdan Chadkin <trysound@yandex.ru>
2017-07-04 10:13:50 +0300

    Add API interfaces

---
+++ src/generators.js
@@ -0,0 +1,13 @@
+// @flow
+
+import type { Constraints, Year, Month, Day } from './types.js';
+
+const getRangeOfYears = (constraints: Constraints): Year[] => {};
+
+const getMonthsOfYear = (constraints: Constraints, date: Date): Month[] => {};
+
+const getDaysOfMonth = (constraints: Constraints, date: Date): Day[] => {};
+
+const getDaysOfNextMonth = (constraints: Constraints, date: Date): Day[] => {};
+
+const getDaysOfPrevMonth = (constraints: Constraints, date: Date): Day[] => {};
